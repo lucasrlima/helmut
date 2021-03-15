f58035beeccd1c979b102cd4ac9782bf3f48d6b3
Lucas Lima <lucasrlima@gmail.com>
2021-03-14 22:02:15 -0300

    Profile Model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_205037) do
+ActiveRecord::Schema.define(version: 2021_03_15_010027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2021_03_14_205037) do
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name_string"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,4 +53,5 @@ ActiveRecord::Schema.define(version: 2021_03_14_205037) do
   end
 
   add_foreign_key "jobs", "users"
+  add_foreign_key "profiles", "users"
 end
