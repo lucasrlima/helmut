ad7884d80948751b43bbbd044db01f6455f3fa8f
Lucas Lima <lucasrlima@gmail.com>
2021-03-15 22:09:55 -0300

    profile create working and authorized

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,18 +3,33 @@ class ProfilesController < ApplicationController
   def new
     @profile = Profile.new
     @profile.user = current_user
+    authorize @profile
   end
 
   def create
     @profile = Profile.new(profile_params)
     @profile.user = current_user
-    @profile.save
+    authorize @profile
+    if @profile.save
+      redirect_to profile_path(@profile)
+    else
+      render :new
+    end
+  end
+
+  def show
+    find_profile
   end
 
   private
 
   def profile_params
-    params.require(:profile).permit(:name, :last_name)
+    params.require(:profile).permit(:first_name, :last_name_string)
+  end
+
+  def find_profile
+    @profile = Profile.find(params[:id])
+    authorize @profile
   end
 
 end
