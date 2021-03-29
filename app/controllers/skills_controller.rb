d31ac6c32bcee6ce6593279de97a7fcbaa77dca4
Lucas Lima <lucasrlima@gmail.com>
2021-03-28 21:08:01 -0300

    New Skill Authorization Done

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,6 +2,8 @@ class SkillsController < ApplicationController
 
     def new
         @profiles = policy_scope(Profile).order(first_name: :asc)
+        @skill = Skill.new
+        authorize @skill
     end
     
 
---
+++ app/policies/skill_policy.rb
@@ -0,0 +1,18 @@
+class SkillPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    is_admin?
+  end
+
+  private
+
+  def is_admin?
+    user.admin
+  end
+
+end
