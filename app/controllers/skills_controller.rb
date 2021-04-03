633ec20b69417a50a7c9e6dc9321ccf1c128d5ca
Lucas Lima <lucasrlima@gmail.com>
2021-04-03 15:26:19 -0300

    create action

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,9 +2,31 @@ class SkillsController < ApplicationController
 
     def new
         @profiles = policy_scope(Profile).order(first_name: :asc)
-        @job = Job.find(params[:job_id])
+        find_job
         @skill = Skill.new
         authorize @skill
+    end
+
+    def create
+        @skill = Skill.new
+        authorize @skill
+        find_job
+        @skill.job = @job
+        
+        if @skill.save
+            redirect_to job_path(@job)
+        end
+
+    end
+    
+    private
+
+    def skill_params
+        params.require(:skill).permit(:job_id, :profile_id)
+    end
+    
+    def find_job
+        @job = Job.find(params[:job_id])
     end
     
 
